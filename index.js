1875d83be0d8c77e76e2a410c1fb97adf52af9a3
Natalia Lima <natalima.al@gmail.com>
2022-08-09 21:33:59 -0300

    aula 2 - carregamento de arquivos

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-const chalk = require('chalk');
+//const chalk = require('chalk');
+const fs = require('fs');
 
-console.log(chalk.blue('vamos lá!!'));
-
-const paragrafo = 'Texto retornado por uma função';
-
-function texto(string) {
-    return string;
+function trataErro(erro) {
+    throw new Error(erro.code, 'não há arquivo no caminho');
 }
 
-console.log(texto(paragrafo));
+// ASYNC AWAIT
+async function pegaArquivo(caminhoDoArquivo) {
+    const enconding = 'utf-8';
+    try {
+        const texto = await fs.promises.readFile(caminhoDoArquivo, enconding)
+        console.log(texto);
+    } catch (erro) {
+        trataErro(erro);
+    }
+}
+
+//function pegaArquivo(caminhoDoArquivo) {
+//    const enconding = 'utf-8';
+//    fs.promises
+//        .readFile(caminhoDoArquivo, enconding)
+//        .then((texto) => console.log(texto))
+//        .catch((erro) => trataErro(erro))
+//}
+
+//function pegaArquivo(caminhoDoArquivo) {
+//    const enconding = 'utf-8';
+//    fs.readFile(caminhoDoArquivo, enconding, (erro, texto) => {
+//        if (erro) {
+//            trataErro(erro);
+//        }
+//        console.log(texto);
+//    })
+//}
+
+pegaArquivo('./arquivos/texto1.md');
